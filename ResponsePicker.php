c0b7eb40df9f1e370d03091cde9876bb2070a1bc
Sam Mousa <sam@mousa.nl>
2015-10-27 10:47:44 +0100

    Updates to look

--- ResponsePicker.php
+++ ResponsePicker.php
@@ -1,4 +1,6 @@
 <?php
+    require_once(__DIR__ . '/vendor/autoload.php');
+
     class ResponsePicker extends \ls\pluginmanager\PluginBase
     {
 
@@ -28,13 +30,9 @@
                     'answer' => $value
                 ];
             }
-            $out .= Yii::app()->controller->widget('zii.widgets.grid.CGridView', [
-                'dataProvider' => new CArrayDataProvider($rows),
-                'columns' => [
-                    'question',
-                    'answer'
-                ]
-                
+            $out .= Yii::app()->controller->widget('zii.widgets.CDetailView', [
+                'data' => $response
+
             ], true);
             $out .= CHtml::link("Back to list", $this->api->createUrl('survey/index', ['sid' => $surveyId, 'token' => $response['token'], 'lang' => 'en', 'newtest' => 'Y']));
             $out .= '</body></html>';
@@ -161,8 +159,20 @@
                         'type' => 'boolean',
                         'label' => 'Use response picker this survey: ',
                         'current' => $this->get('enabled', 'Survey', $event->get('survey'), 0)
-                   ]
-                ]
+                   ],
+                    'columns' => [
+                        'type' => 'text',
+                        'label' => 'Show these columns (One question code per line):',
+                        'current' => $this->get('columns', 'Survey', $event->get('survey'), "")
+                    ],
+                    'newheader' => [
+                        'type' => 'string',
+                        'label' => 'Header for new response button:',
+                        'current' => $this->get('newheader', 'Survey', $event->get('survey'), "New response")
+                    ]
+
+
+                ],
             ];
             $event->set("surveysettings.{$this->id}", $settings);
 
@@ -195,7 +205,9 @@
 //            $result = [];
             foreach ($responses as $response)
             {
+//                echo '<pre>'; var_dump(array_keys($response->attributes)); die();
                 $result[] = [
+//                    'series' => $response->uoid,
                     'data' => $this->api->getResponse($response->surveyId, $response->id),
                     'urls' => [
                         'delete' => $this->api->createUrl('plugins/direct', ['plugin' => __CLASS__, 'function' => 'delete', 'surveyId' => $response->surveyId, 'responseId' => $response->id, 'token' => $response->token]),
@@ -210,8 +222,7 @@
                 'id' => 'new',
                 'url' => $this->api->createUrl('survey/index', $params)
             ];
-//            $this->renderJson($result);
-            $this->renderHtml($result);
+            $this->renderHtml($result, $sid);
         }
 
         protected function renderJson($result) {
@@ -220,30 +231,44 @@
             echo json_encode($result, JSON_PRETTY_PRINT);
             die();
         }
-        
-        protected function renderHtml($result) {
+
+
+        protected function renderHtml($result, $sid) {
             $new = array_pop($result);
             $columns = [];
+//            echo '<pre>';
+//            var_dump($result[0]); die();
             foreach($result as $resultDetails) {
                 foreach($resultDetails['data'] as $key => $value) {
                     $columns[$key] = true;
                 }
             }
             $gridColumns['actions'] = [
+                'header' => 'Actions',
+                'htmlOptions' => [
+                    'width' => '100px',
+                    'style' => 'word-spacing: 2em;'
+                ],
                 'class' => 'CButtonColumn',
                 'template' => '{view}{update}{delete}',
                 'buttons' => [
                     'view' => [
+                        'label' => '<i class="icon-eye-open"></i>',
+                        'imageUrl' => false,
                         'url' => function($data) {
                             return $data['urls']['read'];
                         }
                     ],
                     'update' => [
+                        'label' => '<i class="icon-pencil"></i>',
+                        'imageUrl' => false,
                         'url' => function($data) {
                             return $data['urls']['update'];
                         }
                     ],
                     'delete' => [
+                        'label' => '<i class="icon-trash"></i>',
+                        'imageUrl' => false,
                         'url' => function($data) {
                             return $data['urls']['delete'];
                         }
@@ -252,29 +277,97 @@
             ];
             $gridColumns['id'] = [
                 'name' => 'data.id',
-                'header' => "Response id"
+                'header' => "Response id",
+                'filter'=> false,
+                'htmlOptions' => [
+                    'width' => '100px'
+                ]
+
             ];
             $gridColumns['submitdate'] = [
                 'name' => 'data.submitdate',
-                'header' => "Submit Date"
+                'header' => "Submit Date",
+                'filter'=> 'text',
+                'htmlOptions' => [
+                    'width' => '200px'
+                ]
             ];
+//            $gridColumns['series'] = [
+//                'name' => 'uoid',
+//                'header' => "Series",
+//                'filter'=> 'text',
+//                'htmlOptions' => [
+//                    'width' => '200px'
+//                ]
+//            ];
+            $configuredColumns = explode("\n", $this->get('columns', 'Survey', $sid, ""));
+            foreach($configuredColumns as $column) {
+                if (strpos(':', $column) === false) {
+                    $column .= ':none';
+                }
+                list($name, $filter) = explode(':', $column, 2);
+                $question = Question::model()->findByAttributes([
+                    'sid' => $sid,
+                    'title' => $name
+                ]);
+                if (isset($question)) {
+                    $answers = [];
+                    foreach(Answer::model()->findAllByAttributes([
+                        'qid' => $question->qid,
+                    ]) as $answer) {
+                        $answers[$answer->code] = $answer;
+                    }
+
+//                var_dump($answers); die();
+                }
+                $gridColumns[$name] = [
+                    'name' => "data.$name",
+                    'header' => $question->question,
+                    'filter'=> $filter == 'none' ? false: $filter,
+                ];
+
+                if (isset($answers) && !empty($answers)) {
+                    $gridColumns[$name]['value'] = function($row) use ($answers, $name)
+                    {
+                        return $answers[$row['data'][$name]]->answer;
+                    };
+
+                }
+
+                // Try to get question.
+
+            }
             foreach ($columns as $column => $dummy) {
                 if (substr($column, 0, 4) == 'DISP') {
                     $gridColumns[$column] = [
                         'name' => "data.$column",
-                        'header' => ucfirst($column)
-
+                        'header' => ucfirst($column),
+                        'filter'=> 'select'
                     ];
                 }
             }
-            echo '<html><title></title><body>';
-            Yii::app()->controller->widget('zii.widgets.grid.CGridView', [
-                'dataProvider' => new CArrayDataProvider($result),
+            /** @var \CClientScript $cs */
+            $cs = Yii::app()->clientScript;
+            $cs->reset();
+            \Yii::app()->bootstrap;
+            $output = '<html><title></title><body style="padding: 20px;">';
+            $header = $this->get('newheader', 'Survey', $sid, "New response");
+
+            $output .= \CHtml::link($header, $new['url'], ['class' => 'btn']);
+            $output .= \CHtml::link($header, $new['url'], ['class' => 'btn']);
+            \Yii::import('zii.widgets.grid.CGridView');
+            \Yii::app()->params['bower-asset'] = \Yii::app()->assetManager->publish(__DIR__ . '/vendor/bower-asset');
+            $cs->registerCss('select', 'select { width: 100%; } input[type=text] { height: 30px;}');
+            $output .=  Yii::app()->controller->widget(SamIT\Yii1\DataTables\DataTable::class, [
+                'dataProvider' => new CArrayDataProvider($result, [
+                    'keyField' => false
+                ]),
+                'filter' => true,
                 'columns' => $gridColumns
                 
-            ]);
-            echo CHtml::link("Create new response", $new['url']);
-            die('</body></html>');
+            ], true);
+            $output .= '</body></html>';
+            die($output);
             
         }
         public function newSurveySettings()
